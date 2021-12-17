702c58a99f87eafca93e610465a020a960e9040f
seena <k.a.ebahi@gmail.com>
2021-12-17 23:57:42 +0200

    Fix watch video bug

---
+++ app/src/main/java/com/example/TentwentAssignment/ui/activities/MovieWatchActivity.kt
@@ -0,0 +1,79 @@
+package com.example.TentwentAssignment.ui.activities
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.TentwentAssignment.data.remote.response.movie.video.VideoResponse
+import com.example.TentwentAssignment.databinding.FragmentWatchMovieBinding
+import com.example.TentwentAssignment.domain.viewmodels.MovieViewModel
+import com.example.TentwentAssignment.util.Constants
+import com.example.TentwentAssignment.util.Resource
+import com.google.gson.Gson
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class MovieWatchActivity : AppCompatActivity(){
+
+    val TAG: String = "MovieWatchActivity"
+    lateinit var binding: FragmentWatchMovieBinding
+    val viewModel: MovieViewModel by viewModels()
+    @Inject lateinit var gson: Gson
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentWatchMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.watchBack.setOnClickListener {
+            onBackPressed()
+        }
+        binding.watchDone.setOnClickListener {
+            onBackPressed()
+        }
+        val movieId = intent.getIntExtra(Constants.MOVIE_ID, -1)
+        if (movieId > 0) {
+            viewModel.getVideo(
+                apiKey = Constants.API_KEY,
+                movieId = movieId
+            )
+        }
+        viewModel.videoLiveData.observe(this, {
+            when (it.status) {
+                Resource.Status.LOADING -> {
+                    Log.d(TAG, "LOADING ${it.message}")
+                }
+                Resource.Status.SUCCESS -> {
+                    Log.i(TAG, "SUCCESS ${it.data}")
+                    it.data?.let { videoEntity ->
+                        val videoResponse = gson.fromJson(videoEntity.response, VideoResponse::class.java)
+                        videoResponse?.let { video ->
+                          onPlayVideo(video.results[0].key)
+                        }
+                    }
+                }
+                Resource.Status.ERROR -> {
+                    Log.e(TAG, "ERROR ${it.message}")
+                }
+                else -> {
+                    Log.e(TAG, "INVALID - No status found.")
+                }
+            }
+        })
+    }
+
+    fun onPlayVideo(videoId: String){
+        lifecycle.addObserver(binding.youtubePlayerView)
+        binding.youtubePlayerView.addYouTubePlayerListener(object :
+            AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                youTubePlayer.loadVideo(videoId, 0F)
+            }
+        })
+    }
+
+}
